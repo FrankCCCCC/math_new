8769cd172bdf56ce964f5c2271150aec22f933d3
FrankCCCCC <unaxultraspaceos5@gmail.com>
2021-06-06 22:45:44 +0800

    add wordcloud and Topics

--- statistical_computing/final/SMO/data_analyze.py
+++ statistical_computing/final/SMO/data_analyze.py
@@ -2,144 +2,121 @@
 import numpy as np
 import pandas as pd
 
-import matplotlib.pyplot as plot
+import re
+from bs4 import BeautifulSoup
+import nltk
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
 
 # %%
-df = pd.read_csv('Womens Clothing E-Commerce Reviews.csv')
+def remove_tags(text):
+    # remove HTML tags
+    text = BeautifulSoup(text, 'html.parser').get_text()
+    
+    # regex for matching emoticons, keep emoticons, ex: :), :-P, :-D
+    r = '(?::|;|=|X)(?:-)?(?:\)|\(|D|P)'
+    emoticons = re.findall(r, text)
+    text = re.sub(r, '', text)
+    
+    # convert to lowercase and append all emoticons behind (with space in between)
+    # replace('-','') removes nose of emoticons
+    text = re.sub('[\W]+', ' ', text.lower()) + ' ' + ' '.join(emoticons).replace('-','')
+    return text
+
+nltk.download('stopwords')
+def tokenizer_stem_nostop(text):
+    stop = stopwords.words('english')
+    porter = PorterStemmer()
+    return [porter.stem(w) for w in re.split('\s+', text.strip()) \
+            if w not in stop and re.match('[a-zA-Z]+', w)]
+
+def preprocess(text):
+    text = remove_tags(text)
+    text = tokenizer_stem_nostop(text)
+    text = ' '.join(text)
+    return text
+
+def prep_save():
+    df = pd.read_csv('Womens Clothing E-Commerce Reviews.csv')
+    # Remove Null values
+    df.drop(['Unnamed: 0', 'Title'], axis=1, inplace=True)
+    df.dropna(inplace=True)
+
+    df['Review Text'] = df['Review Text'].apply(preprocess)
+
+    print(df.isnull().sum())
+    df.head()
+    df.to_pickle("clean.pkl")
+
+# %%
+is_read_original = False
+if is_read_original:
+    prep_save()
+else:
+    df = pd.read_pickle("clean.pkl")
+# %%
+# df = df[df["Recommended IND"] == 1]
+# df = df[df["Rating"] == 1]
+
+# df = df[df["Department Name"] == "Intimate"]
+
+# df = df[df["Class Name"] == "Blouses"]
+# df = df[df["Class Name"] == "Bottoms"]
+
+df = df[df["Age"] >= 60]
+# df = df[df["Age"] < 60]
+
 df.head()
 
 # %%
-# Code source: Gaël Varoquaux
-#              Andreas Müller
-# Modified for documentation by Jaques Grobler
-# License: BSD 3 clause
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.pipeline import Pipeline
 
-import numpy as np
+n_comp = 5
+tfidf = TfidfVectorizer(strip_accents=None, lowercase=False, preprocessor=None, norm='l2')
+lda = LatentDirichletAllocation(n_components = n_comp, random_state = 0)
+
+lda_pipe = Pipeline([('tfidf', tfidf), ('lda', lda)])
+
+# %%
+clustered_text = lda_pipe.fit_transform(df['Review Text'])
+# %%
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.datasets import make_moons, make_circles, make_classification
-from sklearn.neural_network import MLPClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+def plot_top_words(model, feature_names, n_top_words, title):
+    fig, axes = plt.subplots(2, 5, figsize=(30, 15), sharex=True)
+    axes = axes.flatten()
+    for topic_idx, topic in enumerate(model.components_):
+        top_features_ind = topic.argsort()[:-n_top_words - 1:-1]
+        top_features = [feature_names[i] for i in top_features_ind]
+        weights = topic[top_features_ind]
 
-h = .02  # step size in the mesh
+        ax = axes[topic_idx]
+        ax.barh(top_features, weights, height=0.7)
+        ax.set_title(f'Topic {topic_idx +1}',
+                     fontdict={'fontsize': 30})
+        ax.invert_yaxis()
+        ax.tick_params(axis='both', which='major', labelsize=20)
+        for i in 'top right left'.split():
+            ax.spines[i].set_visible(False)
+        fig.suptitle(title, fontsize=40)
 
-names = [
-        #  "Nearest Neighbors", 
-         "Linear SVM", 
-         "RBF SVM", 
-        #  "Gaussian Process",
-        #  "Decision Tree", 
-        #  "Random Forest", 
-        #  "Neural Net", 
-        #  "AdaBoost",
-        #  "Naive Bayes", 
-        #  "QDA"
-        ]
+    plt.subplots_adjust(top=0.90, bottom=0.05, wspace=0.90, hspace=0.3)
+    plt.show()
 
-classifiers = [
-    # KNeighborsClassifier(3),
-    SVC(kernel="linear", C=0.025),
-    SVC(gamma=2, C=1),
-    # GaussianProcessClassifier(1.0 * RBF(1.0)),
-    # DecisionTreeClassifier(max_depth=5),
-    # RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
-    # MLPClassifier(alpha=1, max_iter=1000),
-    # AdaBoostClassifier(),
-    # GaussianNB(),
-    # QuadraticDiscriminantAnalysis()
-    ]
+# %%
+pd.DataFrame(clustered_text).head()
 
-X, y = make_classification(n_features=2, n_redundant=0, n_informative=2,
-                           random_state=1, n_clusters_per_class=1)
-rng = np.random.RandomState(2)
-X += 2 * rng.uniform(size=X.shape)
-linearly_separable = (X, y)
+n_top_words = 10
+tf_feature_names = tfidf.get_feature_names()
+plot_top_words(lda, tf_feature_names, n_top_words, 'Topics in LDA model')
+# %%
+from wordcloud import WordCloud
 
-datasets = [make_moons(noise=0.3, random_state=0),
-            make_circles(noise=0.2, factor=0.5, random_state=1),
-            linearly_separable
-            ]
+cloud = WordCloud().generate(" ".join(list(df['Review Text'])))
+cloud.to_file('output.png')
+# %%
 
-figure = plt.figure(figsize=((len(classifiers) + 1)*3, len(datasets)*3))
-i = 1
-# iterate over datasets
-for ds_cnt, ds in enumerate(datasets):
-    # preprocess dataset, split into training and test part
-    X, y = ds
-    X = StandardScaler().fit_transform(X)
-    X_train, X_test, y_train, y_test = \
-        train_test_split(X, y, test_size=.4, random_state=42)
-
-    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
-    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                         np.arange(y_min, y_max, h))
-
-    # just plot the dataset first
-    cm = plt.cm.RdBu
-    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
-    ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
-    if ds_cnt == 0:
-        ax.set_title("Input data")
-    # Plot the training points
-    ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
-               edgecolors='k')
-    # Plot the testing points
-    ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6,
-               edgecolors='k')
-    ax.set_xlim(xx.min(), xx.max())
-    ax.set_ylim(yy.min(), yy.max())
-    ax.set_xticks(())
-    ax.set_yticks(())
-    i += 1
-
-    # iterate over classifiers
-    for name, clf in zip(names, classifiers):
-        ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
-        clf.fit(X_train, y_train)
-        score = clf.score(X_test, y_test)
-
-        # Plot the decision boundary. For that, we will assign a color to each
-        # point in the mesh [x_min, x_max]x[y_min, y_max].
-        if hasattr(clf, "decision_function"):
-            Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
-        elif hasattr(clf, "predict_proba"):
-            Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
-        else:
-            Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-
-        # Put the result into a color plot
-        Z = Z.reshape(xx.shape)
-        ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
-
-        # Plot the training points
-        ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
-                   edgecolors='k')
-        # Plot the testing points
-        ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
-                   edgecolors='k', alpha=0.6)
-
-        ax.set_xlim(xx.min(), xx.max())
-        ax.set_ylim(yy.min(), yy.max())
-        ax.set_xticks(())
-        ax.set_yticks(())
-        if ds_cnt == 0:
-            ax.set_title(name)
-        ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
-                size=15, horizontalalignment='right')
-        i += 1
-
-plt.tight_layout()
-plt.show()
 
 # %%
 
